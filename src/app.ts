3bd4ca716a23c6bfc73fad98f42a968f1f7d9066
Maurício Calil Manfrim Lucera <m.calil-manfrim-lucera@tu-braunschweig.de>
2025-06-08 17:09:02 -0300

    chore: add ESlint and Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
 });
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
-
-const start = async () => {
+const start = async (): Promise<void> => {
   try {
     await fastify.listen({ port: 3000 });
 
-    console.log("Server listening on http://localhost:3000")
+    console.log('Server listening on http://localhost:3000');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-}
+};
 
 start();

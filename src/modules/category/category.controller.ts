100f23612cef052735ae54bea5d56661eb36b8f3
Maurício Calil Manfrim Lucera <m.calil-manfrim-lucera@tu-braunschweig.de>
2025-06-11 11:44:25 -0300

    refactor(category): remove unnecessary parameters in controller.getCategories

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,14 +1,10 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
 import { CategoryService } from './category.service';
 import { GetCategoriesRes, GetCategoriesResSchema } from './category.schema';
 
 export default class CategoryController {
   constructor(private service: CategoryService) {}
 
-  async getCategories(
-    _request: FastifyRequest,
-    _reply: FastifyReply
-  ): Promise<GetCategoriesRes> {
+  async getCategories(): Promise<GetCategoriesRes> {
     const categories = await this.service.getCategories();
     return GetCategoriesResSchema.parse(categories);
   }

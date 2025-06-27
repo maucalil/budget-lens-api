07d426d74f97db20387840bd6e0574d7481cee25
larissa <99204156+lal4mp@users.noreply.github.com>
2025-06-27 17:35:40 -0300

    feat(prisma): add a migration to populate default categories

---
+++ prisma/migrations/20250627195907_populate_categories/migration.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Category" (name, color) VALUES
+  ('Alimentação',     '#c94b62'),
+  ('Assinaturas',     '#6c5ce7'),
+  ('Casa',            '#008b8b'),
+  ('Compras',         '#9d6381'),
+  ('Entradas',        '#07634e'),
+  ('Entretenimento',  '#11238a'),
+  ('Impostos',        '#ef6c00'),
+  ('Outros',          '#3a3a3c'),
+  ('Saúde',           '#43a047'),
+  ('Transporte',      '#394466');
